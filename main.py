2c18caa3a766420b437921f0e96c67908baae5ba
ShaneOgden <sxo6099@rit.edu>
2023-04-18 13:34:48 -0400

    Add basic left right Changes to be committed: 	modified:   main.py 	new file:   resources/bud.jpg

--- main.py
+++ main.py
@@ -9,7 +9,38 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 RESOURCE_PATH = './resources/'
 
-# Press the green button in the gutter to run the script.
+class Player(pygame.sprite.Sprite):
+    """
+    Spawn a player
+    """
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.movex = 0  # move along X
+        self.movey = 0  # move along Y
+        self.frame = 0  # count frames
+        self.images = []
+
+        img = pygame.image.load(RESOURCE_PATH + 'bud.png')
+        self.images.append(img)
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
+
+    def control(self, x, y):
+        """
+        control player movement
+        """
+        self.movex += x
+        self.movey += y
+
+    def update(self):
+        """
+        Update sprite position
+        """
+        self.rect.x = self.rect.x + self.movex
+        self.rect.y = self.rect.y + self.movey
+
+    # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
     pygame.init()
@@ -27,10 +58,15 @@ if __name__ == '__main__':
     BLUE = (0, 0, 255)
     YELLOW = (255, 255, 0)
 
-    char = pygame.image.load(RESOURCE_PATH + 'bud.png')
+    player = Player()
     bg = pygame.image.load(RESOURCE_PATH + 'background.png')
     SCREEN.fill(BLACK)
     pygame.display.flip()
+
+    player.rect.x = 50  # go to x
+    player.rect.y = 50  # go to y
+    player_list = pygame.sprite.Group()
+    player_list.add(player)
 
     is_running = True
     while is_running:
@@ -40,13 +76,16 @@ if __name__ == '__main__':
 
             SCREEN.blit(bg, (0,0))
             pygame.display.update()
-            SCREEN.blit(char, (100, 100))
+            player_list.draw(SCREEN)
             pygame.display.update()
+            player.update()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT or event.key == ord('a'):
                     print('left')
+                    player.control(-5, 0)
                 if event.key == pygame.K_RIGHT or event.key == ord('d'):
                     print('right')
+                    player.control(5, 0)
                 if event.key == pygame.K_UP or event.key == ord('w'):
                     print('jump')
 
@@ -57,6 +96,7 @@ if __name__ == '__main__':
                     print('right stop')
                 if event.key == ord('q'):
                     is_running = False
+
 
     pygame.quit()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
